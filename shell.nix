c40ef988d292c257b309bd357aede1c702d9f63d
mBuschauer <marco@buschauer.us>
2025-01-28 10:19:11 -0500

    create instance

--- shell.nix
+++ shell.nix
@@ -30,12 +30,11 @@ mkShell {
   buildInputs = with pkgs; [ glfw freetype ];
 
 
-  LD_LIBRARY_PATH =
-    "${glfw}/lib:${freetype}/lib:${vulkan-loader}/lib:${vulkan-validation-layers}/lib:${wayland}/lib:${libxkbcommon}/lib";
-
+  LD_LIBRARY_PATH = "${glfw}/lib:${freetype}/lib:${vulkan-loader}/lib:${vulkan-validation-layers}/lib:${wayland}/lib:${libxkbcommon}/lib";
+  
   VULKAN_SDK = "${vulkan-headers}";
   VK_LAYER_PATH = "${vulkan-validation-layers}/share/vulkan/explicit_layer.d";
-  PATH = "${vulkan-tools}/bin:" + builtins.getEnv "PATH";
+  PATH = "${vulkan-tools}/bin:" + builtins.getEnv "PATH"; # sets up vulkan application environment 
 
-  # WINIT_UNIX_BACKEND = "x11";
+  # WINIT_UNIX_BACKEND = "x11"; # to default to x11, didn't create environment variables for
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
+// SPDX-License-Identifier: Apache-2.0
+
 #![allow(
     dead_code,
     unused_variables,
+    clippy::manual_slice_size_calculation,
     clippy::too_many_arguments,
     clippy::unnecessary_wraps
 )]
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use log::*;
+use vulkanalia::loader::{LibloadingLoader, LIBRARY};
+use vulkanalia::prelude::v1_0::*;
+use vulkanalia::window as vk_window;
+use vulkanalia::Version;
 use winit::dpi::LogicalSize;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::EventLoop;
 use winit::window::{Window, WindowBuilder};
 
+/// The Vulkan SDK version that started requiring the portability subset extension for macOS.
+const PORTABILITY_MACOS_VERSION: Version = Version::new(1, 3, 216);
+
+#[rustfmt::skip]
 fn main() -> Result<()> {
     pretty_env_logger::init();
 
@@ -48,12 +60,18 @@ fn main() -> Result<()> {
 
 /// Our Vulkan app.
 #[derive(Clone, Debug)]
-struct App {}
+struct App {
+    entry: Entry,
+    instance: Instance,
+}
 
 impl App {
     /// Creates our Vulkan app.
     unsafe fn create(window: &Window) -> Result<Self> {
-        Ok(Self {})
+        let loader = LibloadingLoader::new(LIBRARY)?;
+        let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
+        let instance = create_instance(window, &entry)?;
+        Ok(Self { entry, instance })
     }
 
     /// Renders a frame for our Vulkan app.
@@ -62,9 +80,41 @@ impl App {
     }
 
     /// Destroys our Vulkan app.
-    unsafe fn destroy(&mut self) {}
+    unsafe fn destroy(&mut self) {
+        self.instance.destroy_instance(None);
+    }
 }
 
 /// The Vulkan handles and associated properties used by our Vulkan app.
 #[derive(Clone, Debug, Default)]
 struct AppData {}
+
+//================================================
+// Instance
+//================================================
+
+unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> {
+    // Application Info
+
+    let application_info = vk::ApplicationInfo::builder()
+        .application_name(b"Vulkan Tutorial (Rust)\0")
+        .application_version(vk::make_version(1, 0, 0))
+        .engine_name(b"No Engine\0")
+        .engine_version(vk::make_version(1, 0, 0))
+        .api_version(vk::make_version(1, 0, 0));
+
+    // Extensions
+
+    let extensions = vk_window::get_required_instance_extensions(window)
+        .iter()
+        .map(|e| e.as_ptr())
+        .collect::<Vec<_>>();
+
+    // Create
+
+    let info = vk::InstanceCreateInfo::builder()
+        .application_info(&application_info)
+        .enabled_extension_names(&extensions);
+
+    Ok(entry.create_instance(&info, None)?)
+}
