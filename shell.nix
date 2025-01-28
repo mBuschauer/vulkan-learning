f42eb378f8757f26424e36119629e17b16e0e125
mBuschauer <marco@buschauer.us>
2025-01-28 09:44:12 -0500

    first commit

---
+++ shell.nix
@@ -0,0 +1,41 @@
+with import <nixpkgs> { };
+
+mkShell {
+  name = "vulkan-dev";
+  packages = [
+    rustup
+    openssl
+    pkg-config
+
+    xorg.libX11
+    xorg.libXau
+    xorg.libXdmcp
+
+    wayland
+    wayland-protocols
+    libxkbcommon
+
+    glfw
+    freetype
+    vulkan-headers
+    vulkan-loader
+    vulkan-validation-layers
+    vulkan-tools # vulkaninfo
+    shaderc # GLSL to SPIRV compiler - glslc
+    renderdoc # Graphics debugger
+    tracy # Graphics profiler
+    vulkan-tools-lunarg # vkconfig
+  ];
+
+  buildInputs = with pkgs; [ glfw freetype ];
+
+
+  LD_LIBRARY_PATH =
+    "${glfw}/lib:${freetype}/lib:${vulkan-loader}/lib:${vulkan-validation-layers}/lib:${wayland}/lib:${libxkbcommon}/lib";
+
+  VULKAN_SDK = "${vulkan-headers}";
+  VK_LAYER_PATH = "${vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+  PATH = "${vulkan-tools}/bin:" + builtins.getEnv "PATH";
+
+  # WINIT_UNIX_BACKEND = "x11";
+}
